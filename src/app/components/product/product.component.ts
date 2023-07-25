06775fdbcc222cf1360de19c5a95bfad6870db72
yusufislamarslanturk <ysarslanturk@gmail.com>
2023-07-25 12:56:49 +0300

    .

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+ 
+
+  products:Product[] = [];
+
+}
