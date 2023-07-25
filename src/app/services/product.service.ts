9dbd7a3449c3c3e83783d1325fdae2ba16083d36
yusufislamarslanturk <ysarslanturk@gmail.com>
2023-07-25 15:27:05 +0300

    17. ders sonu

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from '../models/productResponseModel';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  apiUrl = 'https://localhost:44371/api/products/getall';
+
+  constructor(private httpClient: HttpClient) {}
+  getProducts(): Observable<ProductResponseModel> {
+    return this.httpClient.get<ProductResponseModel>(this.apiUrl);
+  }
+}
